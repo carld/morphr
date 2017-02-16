7871d736538e427db1cfff58aaef1aa3705d1062
Markus Voge <markus.voge@ea-aw.de>
2017-02-16 12:39:08 +0100

    Add functions to handle the morphological field in Shiny.

---
+++ R/morphr-shiny.R
@@ -0,0 +1,93 @@
+#' Helper functions for using morphr in Shiny
+#'
+#' If you want to use the built-in functionality, you must use the function
+#' \code{installMorphField} instead of \code{output$mf <- renderMorphField(...)}.
+#' This is because it makes use of shiny's reactivity, so it needs to install
+#' the morph field widget in the output object and observe the input object.
+#' Besides that, there are the two usual functions \code{fooOutput()} and
+#' \code{renderFoo()} typical for the \pkg{shiny} package. The former is used to
+#' create a container for the morphological field, and the latter is used in the
+#' server logic to render the field.
+#' @export
+installMorphField <- function(input, output, session, id,
+                              param_values, specific_configurations = NULL,
+                              styleFunc = NULL) {
+  param_values <- paramValuesToDataFrame(param_values)
+
+  field <- morphfield(param_values, specific_configurations)
+  if (!is.null(styleFunc)) {
+    field <- styleFunc(field)
+  }
+  output[[id]] <- renderMorphField(
+    field
+  )
+
+  proxy <- dataTableProxy(id)
+
+  # immediately deselect empty cells, they shall not be selectable
+  observeEvent(input[[paste0(id, "_cells_selected")]], {
+    cells <- input[[paste0(id, "_cells_selected")]]
+    last_selected_cell <- cells[nrow(cells),]
+    row <- last_selected_cell[1]
+    col <- last_selected_cell[2]
+    cell_content <- param_values[row, col + 1]
+    if (is.null(cell_content) || cell_content == "") {
+      proxy %>% selectCells(cells[-nrow(cells),])
+    }
+  })
+
+  if (!is.null(specific_configurations)) {
+    # enforce the specific configurations for automatic cell selection
+    observeEvent(input[[paste0(id, "_cells_selected")]], {
+      cells <- input[[paste0(id, "_cells_selected")]]
+      last_selected_cell <- cells[nrow(cells),]
+      row <- last_selected_cell[1]
+      col <- last_selected_cell[2]
+      determined_cells <- determineCells(param_values, specific_configurations,
+                                         row, col)
+      if (!is.null(determined_cells)) {
+        proxy %>% selectCells(determined_cells)
+      }
+    })
+  }
+
+  return(proxy)
+}
+
+
+#' @rdname installMorphField
+#' @inheritParams shiny::textOutput
+#' @export
+morphFieldOutput <- function(outputId, width = '100%', height = 'auto') {
+  htmltools::attachDependencies(
+    htmlwidgets::shinyWidgetOutput(
+      outputId, 'datatables', width, height, package = 'morphr'
+    ),
+    c(
+      crosstalk::crosstalkLibs(),
+      list(htmltools::htmlDependency(
+        "morphr-css", "0.0.1",
+        c(file = system.file("htmlwidgets", package = "morphr")),
+        stylesheet = "css/morphr.css"
+      ))
+    ),
+    append = TRUE
+  )
+}
+
+
+#' @rdname installMorphField
+#' @inheritParams shiny::renderText
+#' @inheritParams renderDataTable
+#' @export
+renderMorphField <- function(expr, server = TRUE, env = parent.frame(),
+                             quoted = FALSE, ...) {
+  # if (!quoted) expr = substitute(expr)
+  # shinyRenderWidget(expr, sigmaOutput, env, quoted = TRUE)
+  # shiny::markRenderFunction(morphFieldOutput, function(shinysession, name, ...) {
+  #   print(names(shinysession))
+  #   print(names(shinysession$output))
+  #   print("Hallo Welt!")
+  # })
+  renderDataTable(expr = expr, server = server, quoted = quoted, ...) # must not give the default env (parent.frame()) to renderDataTable, or the expr will not be found in scope
+}
--- R/morphr.R
+++ R/morphr.R
@@ -4,8 +4,52 @@
 #' of table where each column represents the possible configurations of a
 #' certain parameter) using the JavaScript library DataTables.
 #' @export
-morphfield <- function(input, output, session, id,
-                       param_values, specific_configurations = NULL) {
+morphfield <- function(param_values, specific_configurations = NULL) {
+  param_values <- paramValuesToDataFrame(param_values)
+
+  field <- datatable(
+    param_values,
+    options = list(
+      # Disable search/filter box:
+      searching = FALSE,
+      # Disable sorting by columns for all columns:
+      columnDefs = list(
+        list(orderable = FALSE, targets = 0:(ncol(param_values) - 1))
+      ),
+      # Show all rows on one page and no navigation:
+      paging = FALSE,
+      # Do not show info about data in footer:
+      info = FALSE
+    ),
+    # Disable display of rownames:
+    rownames = FALSE,
+    # Enable cell selection:
+    selection = list(
+      mode = "multiple",
+      target = "cell"
+    )
+  )
+
+  if (!is.null(specific_configurations)) {
+    # Style the parameters that define specific configurations differently:
+    field <- field %>%
+      formatStyle(names(specific_configurations),
+                  backgroundColor = "gray")
+  }
+
+  return(field)
+}
+
+
+#' Create a Fixed Dimension data.frame from Variable Length Lists of Morphological Field Parameter Values
+#'
+#' A morphological field can consist of parameters with variable number of
+#' configurations. However, a DataTable accepts only a data.frame, which must
+#' have the same number of rows in each column. Therefore, this function takes
+#' a list of lists (each list representing one list of parameter configurations,
+#' i.e. one column) and fills the empty cells with empty character strings to
+#' create a fixed dimension data.frame.
+paramValuesToDataFrame <- function(param_values) {
   if (class(param_values) == "list") {
     # Make sure that all list items have same length.
     # If not: fill with empty character strings
@@ -16,47 +60,7 @@ morphfield <- function(input, output, session, id,
     # Turn it into a data.frame, because that is what is expected by DT
     param_values <- as.data.frame(param_values)
   }
-
-  output[[id]] <- renderDataTable(
-    datatable(
-      param_values,
-      options = list(
-        # Disable search/filter box:
-        searching = FALSE,
-        # Disable sorting by columns for all columns:
-        columnDefs = list(
-          list(orderable = FALSE, targets = 0:(ncol(param_values) - 1))
-        ),
-        # Show all rows on one page and no navigation:
-        paging = FALSE,
-        # Do not show info about data in footer:
-        info = FALSE
-      ),
-      # Disable display of rownames:
-      rownames = FALSE,
-      # Enable cell selection:
-      selection = list(
-        mode = "multiple",
-        target = "cell"
-      )
-    )
-  )
-
-  if (!is.null(specific_configurations)) {
-    proxy <- dataTableProxy(id)
-
-    observeEvent(input[[paste0(id, "_cells_selected")]], {
-      cells <- input[[paste0(id, "_cells_selected")]]
-      last_selected_cell <- cells[nrow(cells),]
-      row <- last_selected_cell[1]
-      col <- last_selected_cell[2]
-      determined_cells <- determineCells(param_values, specific_configurations,
-                                         row, col)
-      if (!is.null(determined_cells)) {
-          proxy %>% selectCells(determined_cells)
-      }
-    })
-  }
+  param_values
 }
 
 
