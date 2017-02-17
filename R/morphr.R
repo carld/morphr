770095f1f0d10e8f226c1b1d8ed30af56f7a00ad
Markus Voge <markus.voge@ea-aw.de>
2017-02-17 21:23:19 +0100

    New functions buildHashValue() and buildUnconstrainedCCM().

--- R/morphr.R
+++ R/morphr.R
@@ -3,18 +3,32 @@
 #' This function creates an HTML widget to display a morphological field (a kind
 #' of table where each column represents the possible configurations of a
 #' certain parameter) using the JavaScript library DataTables.
+#' @param param_values A named list of vectors/lists. The names of the list are
+#'   the names of the parameters (columns) in the morphological field. The
+#'   vectors/lists contain the possible values that the parameter can have.
+#' @param specific_configurations Optional. One can define one or more parameter
+#'   columns to \emph{specify} the field. This means that choosing a value in
+#'   the specifying column sets the values of some or all other columns. It
+#'   constrains the field. The \code{specific_configurations} are expected to be
+#'   a named list of named lists of named lists. The top-level hierarchy names
+#'   represent the parameter names that are specifying. The next level names
+#'   represent the parameter's values that are specifying. The deepest level
+#'   names represent the parameter names that are specified and the values
+#'   (list elements) are the parameter value(s) that are specified (i.e.
+#'   considered possible) in this configuration. Defaults to NULL, i.e. no
+#'   specification and the field is \emph{open}.
 #' @export
 morphfield <- function(param_values, specific_configurations = NULL) {
-  param_values <- paramValuesToDataFrame(param_values)
+  field_df <- paramValuesToDataFrame(param_values)
 
   field <- datatable(
-    param_values,
+    field_df,
     options = list(
       # Disable search/filter box:
       searching = FALSE,
       # Disable sorting by columns for all columns:
       columnDefs = list(
-        list(orderable = FALSE, targets = 0:(ncol(param_values) - 1))
+        list(orderable = FALSE, targets = 0:(ncol(field_df) - 1))
       ),
       # Show all rows on one page and no navigation:
       paging = FALSE,
@@ -34,48 +48,49 @@ morphfield <- function(param_values, specific_configurations = NULL) {
     # Style the parameters that define specific configurations differently:
     field <- field %>%
       formatStyle(names(specific_configurations),
-                  backgroundColor = "gray")
+                  color = "white", backgroundColor = "gray")
   }
 
   return(field)
 }
 
 
-#' Create a Fixed Dimension data.frame from Variable Length Lists of Morphological Field Parameter Values
+#' Create a fixed dimension data.frame from variable length lists of morphological field parameter values
 #'
 #' A morphological field can consist of parameters with variable number of
-#' configurations. However, a DataTable accepts only a data.frame, which must
+#' values. However, a DataTable accepts only a data.frame, which must
 #' have the same number of rows in each column. Therefore, this function takes
 #' a list of lists (each list representing one list of parameter configurations,
 #' i.e. one column) and fills the empty cells with empty character strings to
 #' create a fixed dimension data.frame.
 paramValuesToDataFrame <- function(param_values) {
-  if (class(param_values) == "list") {
+  ret_val <- param_values
+  if (class(ret_val) == "list") {
     # Make sure that all list items have same length.
     # If not: fill with empty character strings
-    max_length <- max(sapply(param_values, length))
-    param_values <- lapply(param_values, function(li) {
+    max_length <- max(sapply(ret_val, length))
+    ret_val <- lapply(ret_val, function(li) {
       c(li, rep("", max_length - length(li)))
     })
     # Turn it into a data.frame, because that is what is expected by DT
-    param_values <- as.data.frame(param_values)
+    ret_val <- as.data.frame(ret_val)
   }
-  param_values
+  ret_val
 }
 
 
-determineCells <- function(param_values, specific_configurations, row, col) {
-  col_names <- names(param_values)
+determineCells <- function(field_df, specific_configurations, row, col) {
+  col_names <- names(field_df)
   col_name <- col_names[col + 1] # + 1 needed because JavaScript starts counting at 0
   determined_cells <- NULL
   if (col_name %in% names(specific_configurations)) {
-    row_name <- param_values[row, col + 1] # + 1 see above
+    row_name <- field_df[row, col + 1] # + 1 see above
     if (row_name %in% names(specific_configurations[[col_name]])) {
       determined_col_names <- names(specific_configurations[[col_name]][[row_name]])
       determined_cols <- match(determined_col_names, col_names)
       determined_row_names <- specific_configurations[[col_name]][[row_name]]
       determined_rows <- sapply(determined_col_names, function(c) {
-        match(determined_row_names[[c]], param_values[[c]])
+        match(determined_row_names[[c]], field_df[[c]])
       })
       determined_cells <- matrix(
         c(row, determined_rows, col, determined_cols - 1), # - 1 needed because JavaScript starts counting at 0
@@ -84,4 +99,73 @@ determineCells <- function(param_values, specific_configurations, row, col) {
     }
   }
   determined_cells
+}
+
+
+#' Build a single hash value from two parameter/value combinations
+#'
+#' This function simply combines four strings (with two strings forming a pair
+#' consisting of a parameter and a value) into a single one. It does so in a
+#'way that the order of the parameter-value combinations does not matter.
+#' Switching param1/value1 pair and the param2/value2 pair will result in the
+#' same hash character string.
+#'
+#' The purpose of this function is to provide hash keys that can be used to
+#' index a named list representing the CCM (cross-consistency matrix) of a
+#' morphological field. The CCM contains a logical for each pair-wise comparison
+#' of the parameter values of a morphological field, being TRUE if the
+#' combination of the two values is consistent or valid, otherwise FALSE. It is
+#' a symmetric matrix with empty diagonal. This means that the result of
+#' assessing the consistency of value1 in param1 (column1 of the morphological
+#' field) with value2 in param2 is the same as the consistency of value2 in
+#' param2 with value1 in param1 (hence the symmetry). Parameter values are not
+#' cross-checked with other values of the same parameter (hence the empty
+#' diagonal).
+#'
+#' The CCM is stored as a list whose names are hash strings of the two compared
+#' parameter/value pairs. One could also store the CCM as a simple matrix, but
+#' this has two drawbacks: (1) Even though it's declare to make a matrix as
+#' symmetric with \code{\link{Matrix::forceSymmetric}()}, this does not appear
+#' to make the matrix \emph{stay} symmetric when it is modified, i.e. updating
+#' the upper/lower triangle does not change the lower/upper triangle
+#' accordingly. One must therefore decide for one half of the matrix and
+#' remember to operate only on that. (2) It is slightly less straight forward to
+#' look up the consistency for a given pair of values. One must first find the
+#' indices corresponding to them. (However, this would be more trivial when the
+#' matrix columns/rows are named.) I find it a bit easier to use
+#' \code{\link{buildHashValue}()} on the value pair and use the hash for the
+#' lookup in a named list, where one does not need to remember which value is
+#' column, which is row.
+#' @param param1 One of the two morphological field parameters to look up the
+#'   consistency for. It corresponds to one column in the morphological field.
+#' @param value1 The value within \code{param1} to look up consistency for. It
+#'   belongs to one cell in the morphological field.
+#' @param param2 Like \code{param1}, but for the other side of the comparison.
+#' @param value2 Like \code{value1}, but for the other side of the comparison.
+#' @return A single character string, which is the sorted hash of the value
+#'   combination for lookup in the CCM.
+#' @export
+buildHashValue <- function(param1, value1, param2, value2) {
+  paste(sort(c(
+    paste(param1, value1, sep = "->"),
+    paste(param2, value2, sep = "->")
+  )), sep = "->", collapse = "|")
+}
+
+
+#' @export
+buildUnconstrainedCCM <- function(param_values) {
+  ccm <- list()
+  lapply(1:(length(param_values) - 1), function(i) {
+    param1 <- names(param_values)[i]
+    lapply(param_values[[param1]], function(value1) {
+      lapply((i + 1):length(param_values), function(j) {
+        param2 <- names(param_values)[j]
+        lapply(param_values[[param2]], function(value2) {
+          ccm[[buildHashValue(param1, value1, param2, value2)]] <<- TRUE
+        })
+      })
+    })
+  })
+  ccm
 }
